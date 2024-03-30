8920fcc2000e693d0f07252833088a9fa8b89842
BrijeshNMeena <brijeshmeena1606@gmail.com>
2024-03-31 01:11:32 +0530

    ready to deploy

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,4 +1,4 @@
-import genres from "../data/Genres";
+import genres from "../data/genres";
 import useData from "./useData";
 
 export interface Genre{
@@ -9,7 +9,7 @@ export interface Genre{
 
 
 
-// const useGenres = () => useData<Genre>('/genres');
-const useGenres = () => ({data: genres, isLoading: false, error: null});
+const useGenres = () => useData<Genre>('/genres');
+// const useGenres = () => ({data: genres, isLoading: false, error: null});
 
 export default useGenres;
